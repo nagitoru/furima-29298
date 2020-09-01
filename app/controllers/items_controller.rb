280b25836ea60b472709c935aae1575777eccded
nagitoru <yokuha188ku@gmail.com>
2020-09-01 18:29:19 +0900

    バリデーションと保存機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,4 +7,19 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name,:image,:text,:genre_id,:status_id,:postage_id,:place_id,:delivery_day_id,:price).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,19 @@ class Item < ApplicationRecord
   belongs_to_active_hash :postage
   belongs_to_active_hash :place
   belongs_to_active_hash :delivery_day
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :text
+    validates :price, format: {with: /\A[0-9]+\z/, message: "Harf-width number"}
+  end
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "Out of setting range"}
+  with_options numericality: { other_than: 1, message: "can't be blank" } do
+    validates :genre_id
+    validates :status_id
+    validates :postage_id
+    validates :place_id
+    validates :delivery_day_id
+  end
 end
