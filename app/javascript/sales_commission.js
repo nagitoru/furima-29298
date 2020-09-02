8c0edbdcbdb8803de6741f436cb2785a7bb0af39
nagitoru <yokuha188ku@gmail.com>
2020-09-02 17:44:52 +0900

    販売価格の非同期的なものの実装

---
+++ app/javascript/sales_commission.js
@@ -0,0 +1,22 @@
+function salesCommission() {
+  clearInterval(salesCommission)
+  const itemPrice = document.getElementById("item-price");
+  const addTaxPrice = document.getElementById('add-tax-price');
+  const profit = document.getElementById('profit');
+
+  itemPrice.addEventListener('input', function() {
+    const price = itemPrice.value
+    if (price.match(/^[0-9]*$/)) {
+      tax = price / 10 | 0;
+      addTaxPrice.innerHTML = tax;
+      pro = price * 9 / 10 | 0;
+      profit.innerHTML = pro;
+    } else {
+      word = "NaN";
+      addTaxPrice.innerHTML = word;
+      profit.innerHTML = word;
+    }
+  });
+}
+clearInterval(salesCommission)
+setInterval(salesCommission, 1000);
