5a82a9ef8a87e4b0abc537c8e820943b82e807fa
nagitoru <yokuha188ku@gmail.com>
2020-09-13 12:08:25 +0900

    commentの機能

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'コメントの保存' do
+    before do
+      @user = FactoryBot.create(:user)
+      @comment = FactoryBot.build(:comment)
+    end
+    it '保存できるとき' do
+      expect(@comment).to be_valid
+    end
+    it '保存できないとき' do
+      @comment.text = ""
+      @comment.valid?
+      expect(@comment.errors.full_messages).to include("Text can't be blank")
+    end
+  end
 end
