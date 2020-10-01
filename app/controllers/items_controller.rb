11a6d0b57dec8d94a07463dde3cc8fba8825d0c4
nagitoru <yokuha188ku@gmail.com>
2020-10-01 20:02:56 +0900

    購入後に編集できないようにする機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,7 +25,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    redirect_to root_path unless current_user == @item.user
+    redirect_to root_path unless current_user == @item.user && @item.order == nil
   end
 
   def update
